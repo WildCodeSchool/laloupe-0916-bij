a72e74736fb6c69c33a749247257313aac7d7c6f
stephsen <stephsen59@gmail.com>
2016-11-30 23:43:20 +0100

    merge conflics

--- public/js/controllers/parcoursController.js
+++ public/js/controllers/parcoursController.js
@@ -1,10 +1,3 @@
 function parcoursController() {
 
-    this.parcoursService = parcoursService;
-    this.load();
-  load() {
-    this.parcoursService.gatAll().then((res) => {
-      this.parcourss=res.data;
-    });
-  }
 }

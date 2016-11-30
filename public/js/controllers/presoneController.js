f79d3d80e03d048d3c7245f8dcdf4d7dc4ec1bda
stephsen <stephsen59@gmail.com>
2016-11-30 23:29:38 +0100

    navigation finish

--- public/js/controllers/presoneController.js
+++ public/js/controllers/presoneController.js
@@ -1,48 +1,60 @@
 function presoneController() {
-  console.log("pass");
-  this.slides = [
-    {
-      image: '1'
-    },
-    {
-      image: '2'
-    },
-    {
-      image: '3'
-    },
-    {
-      image: '4'
-    },
-    {
-      image: '5'
-    },
-    {
-      image: '6'
-    },
-    {
-      image: '7'
-    },
-    {
-      image: '8'
-    },
-    {
-      image: '9'
-    },
-    {
-      image: '10'
-    }
-  ];
-  index = 0;
-  this.preview = () => {
-
-    this.moveStyle = "margin-left: 25vw;";
-  };
-
-  this.next = () => {
-    this.selected = angular.element(document.querySelector('#'+index)).addClass('selected');
-    console.log(this.selected);
-    this.moveStyle = "margin-left: -25vw;";
-    index++;
-
-  };
+    console.log("pass");
+    this.slides = [{
+        image: '1'
+    }, {
+        image: '2'
+    }, {
+        image: '3'
+    }, {
+        image: '4'
+    }, {
+        image: '5'
+    }, {
+        image: '6'
+    }, {
+        image: '7'
+    }, {
+        image: '8'
+    }, {
+        image: '9'
+    }, {
+        image: '10'
+    }];
+    index = 0;
+    position = 0;
+    this.preview = () => {
+        if (index > 0) {
+            this.value = index + 3;
+            if (angular.element(document.querySelector('#b' + this.value))[0].className == "active") {
+              this.active = this.value;
+              // console.log(this.active);
+              angular.element(document.querySelector('#b' + this.value)).removeClass('active');
+            }
+            position = position - 25;
+            this.moveStyle = "margin-left: -" + position + "vw;";
+            index--;
+            // console.log(index);
+            // console.log(position);
+        }
+    };
+    this.next = () => {
+        if (index < this.slides.length - 4) {
+            angular.element(document.querySelector('#b' + this.active)).addClass('active');
+            position = position + 25;
+            this.moveStyle = "margin-left: -" + position + "vw;";
+            index++;
+            // console.log(index);
+            // console.log(position);
+        }
+    };
+    this.select = (index) => {
+      if (angular.element(document.querySelector('#b' + index))[0].className != "active" && this.nextIndex == index) {
+        this.elAdd = angular.element(document.querySelector('#b' + index)).addClass('active');
+      } else {
+        this.elAdd = angular.element(document.querySelector('#b' + index)).addClass('active');
+        this.elRemove = angular.element(document.querySelector('#b' + this.nextIndex)).removeClass('active');
+        this.nextIndex = index;
+      }this.active = index;
+    };
 }

bd32f274a146cdc40d27f11f5d9e13232338dd3a
stephsen <stephsen59@gmail.com>
2016-12-03 15:34:49 +0100

    amination ouverture presentation

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute'])
+angular.module('app', ['ngRoute', 'ngAnimate'])
         .controller('mainController', mainController)
         .controller('presoneController', presoneController)
         .controller('parcoursController', parcoursController)

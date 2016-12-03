3d669adadab357606d9942ce126314c894acc58c
stephsen <stephsen59@gmail.com>
2016-12-03 14:21:45 +0100

    fix bug include index1

--- public/js/controllers/presoneController.js
+++ public/js/controllers/presoneController.js
@@ -1,25 +1,25 @@
 function presoneController() {
-    this.includerPres1 = 'views/pres1/views/sheet1.html';
+    this.includerPres1 = '1';
     this.slides = [{
-        image: '1'
+        image: '1', title: '24h en images', logo:''
     }, {
-        image: '2'
+        image: '2', title: '24h en images', logo:''
     }, {
-        image: '3'
+        image: '3', title: '24h en images', logo:''
     }, {
-        image: '4'
+        image: '4', title: 'Exercices: Légendage', logo:''
     }, {
-        image: '5'
+        image: '5', title: '24h en images', logo:''
     }, {
-        image: '6'
+        image: '6', title: '24h en images', logo:''
     }, {
-        image: '7'
+        image: '7', title: 'Exercices: Recadrage', logo:''
     }, {
-        image: '8'
+        image: '8', title: 'International', logo:''
     }, {
-        image: '9'
+        image: '9', title: 'Exercices: Putaclic', logo:''
     }, {
-        image: '10'
+        image: '10', title: 'Exercices: Putaclic', logo:''
     }];
     this.index = 0;
     this.position = 0;
@@ -34,18 +34,16 @@ function presoneController() {
             this.position = this.position - 25;
             this.moveStyle = "margin-left: -" + this.position + "vw;";
             this.index--;
-            // console.log(this.index);
-            // console.log(this.position);
         }
     };
     this.next = () => {
         if (this.index < this.slides.length - 4) {
-            angular.element(document.querySelector('#b' + this.active)).addClass('active');
+            if (this.index + 4 == this.active) {
+              angular.element(document.querySelector('#b' + this.active)).addClass('active');
+            }
             this.position = this.position + 25;
             this.moveStyle = "margin-left: -" + this.position + "vw;";
             this.index++;
-            // console.log(this.index);
-            // console.log(this.position);
         }
     };
     this.select = (index) => {
@@ -57,7 +55,8 @@ function presoneController() {
         this.nextIndex = index;
       }
       this.active = index;
-      this.includerPres1 = 'views/pres1/views/sheet' + index + '.html';
-
+      this.includerPres1 =  index+1;
+      console.log(index+1);
+      console.log(this.includerPres1);
     };
 }

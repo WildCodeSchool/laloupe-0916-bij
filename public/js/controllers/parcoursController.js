e7a8d542e6562b7de5337c565c2f63ac44217381
daedalus418 <morin.gaelle@gmail.com>
2016-12-06 15:15:25 +0100

    summaries added, parcoursController refactored

--- public/js/controllers/parcoursController.js
+++ public/js/controllers/parcoursController.js
@@ -8,19 +8,11 @@ function parcoursController() {
     getElement('#padlock'+value).removeClass('lock icon');
     getElement('#resume'+value).removeClass('hide');
     getElement('#padlock'+value).addClass('unlock icon');
-
-    // angular.element(document.querySelector('#padlock'+value)).removeClass('lock icon');
-    // angular.element(document.querySelector('#resume'+value)).removeClass('hide');
-    // angular.element(document.querySelector('#padlock'+value)).addClass('unlock icon');
   };
   this.lock=(value)=>{
     getElement('#padlock'+value).removeClass('unlock icon');
     getElement('#padlock'+value).addClass('lock icon');
     getElement('#resume'+value).addClass('hide');
-
-    // angular.element(document.querySelector('#padlock'+value)).removeClass('unlock icon');
-    // angular.element(document.querySelector('#padlock'+value)).addClass('lock icon');
-    // angular.element(document.querySelector('#resume'+value)).addClass('hide');
   };
 
   this.crayon = 'edit icon';

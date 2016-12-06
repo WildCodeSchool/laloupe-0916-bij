ea7dc3e8e35abfe0ae3c85508fed797bb8dea0af
daedalus418 <morin.gaelle@gmail.com>
2016-12-06 15:06:44 +0100

    summaries added, parcoursController refactored

--- public/js/controllers/parcoursController.js
+++ public/js/controllers/parcoursController.js
@@ -1,11 +1,26 @@
 function parcoursController() {
+
+  function getElement(id){
+    return angular.element(document.querySelector(id));
+  }
+
   this.unlock=(value)=>{
-    angular.element(document.querySelector('#padlock'+value)).removeClass('lock icon');
-    angular.element(document.querySelector('#padlock'+value)).addClass('unlock icon');
+    getElement('#padlock'+value).removeClass('lock icon');
+    getElement('#resume'+value).removeClass('hide');
+    getElement('#padlock'+value).addClass('unlock icon');
+
+    // angular.element(document.querySelector('#padlock'+value)).removeClass('lock icon');
+    // angular.element(document.querySelector('#resume'+value)).removeClass('hide');
+    // angular.element(document.querySelector('#padlock'+value)).addClass('unlock icon');
   };
   this.lock=(value)=>{
-    angular.element(document.querySelector('#padlock'+value)).removeClass('unlock icon');
-    angular.element(document.querySelector('#padlock'+value)).addClass('lock icon');
+    getElement('#padlock'+value).removeClass('unlock icon');
+    getElement('#padlock'+value).addClass('lock icon');
+    getElement('#resume'+value).addClass('hide');
+
+    // angular.element(document.querySelector('#padlock'+value)).removeClass('unlock icon');
+    // angular.element(document.querySelector('#padlock'+value)).addClass('lock icon');
+    // angular.element(document.querySelector('#resume'+value)).addClass('hide');
   };
 
   this.crayon = 'edit icon';

b3e84abf95b3aea79dba27698378997665b66f9b
stephsen <stephsen59@gmail.com>
2016-12-12 23:00:44 +0100

    syncro navigation slide with next button

--- public/js/controllers/presoneController.js
+++ public/js/controllers/presoneController.js
@@ -1,9 +1,10 @@
 function presoneController() {
-    angular.element(document.querySelector('#b0')).addClass('active');
+    // angular.element(document.querySelector('#b0')).addClass('active pos1');
     this.show = false;
     this.includerPres1 = '1';
     this.active = 0;
     this.selectClass = "1";
+    this.light = false;
     this.slides = [{
         image: '1_chien_small',
         title: '24h en images',
@@ -85,46 +86,65 @@ function presoneController() {
         icon: './assets/icon/end_turquoise.png',
         color: 'turquoise'
     }];
-    this.index = 0;
-    this.position = 0;
-    this.preview = () => {
-        if (this.index > 0) {
-            this.value = this.index + 3;
-            if (angular.element(document.querySelector('#b' + this.value))[0].className == "active") {
-                this.active = this.value;
-                // console.log(this.active);
-                angular.element(document.querySelector('#b' + this.value)).removeClass('active');
-            }
-            this.position = this.position - 25;
-            this.moveStyle = "margin-left: -" + this.position + "vw;";
-            this.index--;
-        }
-    };
-    this.next = () => {
-        if (this.index < this.slides.length - 4) {
-            if (this.index + 4 == this.active) {
-                angular.element(document.querySelector('#b' + this.active)).addClass('active');
-            }
-            this.position = this.position + 25;
-            this.moveStyle = "margin-left: -" + this.position + "vw;";
-            this.index++;
-        }
-    };
-    this.select = (index) => {
-      if (angular.element(document.querySelector('#b' + index))[0].className != "active" && this.active == index) {
-        angular.element(document.querySelector('#b' + index)).addClass('active');
-      } else {
-        angular.element(document.querySelector('#b' + index)).addClass('active');
-        angular.element(document.querySelector('#b' + this.active)).removeClass('active');
-        this.active = index;
-      }
-      this.selectClass = Math.floor( Math.random() * (4 - 1) + 1 );
-      this.active = index;
-      this.includerPres1 =  index+1;
-    };
-    this.nextSlide = () => {
-      this.includerPres1++;
-    };
+    // this.index = 0;
+    // this.active = 0;
+    // this.previewPos = 0;
+    // this.position = 0;
+    // this.preview = () => {
+    //     if (this.index > 0) {
+    //         this.value = this.index + 3;
+    //         if (angular.element(document.querySelector('#b' + this.value))[0].className == "active") {
+    //             this.active = this.value;
+    //             // console.log(this.active);
+    //             angular.element(document.querySelector('#b' + this.active)).removeClass('active');
+    //         }
+    //         this.position = this.position - 25;
+    //         this.moveStyle = "margin-left: -" + this.position + "vw;";
+    //         this.previewPos = this.index;
+    //         this.index--;
+    //     }
+    // };
+    // this.next = () => {
+    //     if (this.index < this.slides.length - 4) {
+    //         if (this.index + 4 == this.active) {
+    //             angular.element(document.querySelector('#b' + this.active)).addClass('active');
+    //         }
+    //         this.position = this.position + 25;
+    //         this.moveStyle = "margin-left: -" + this.position + "vw;";
+    //         this.previewPos = this.index;
+    //         this.index++;
+    //     }
+    // };
+    // this.select = (index) => {
+    //   this.previewPos = this.active;
+    //   if (angular.element(document.querySelector('#b' + index))[0].className != "active" && this.active == index) {
+    //     angular.element(document.querySelector('#b' + index)).addClass('active');
+    //     this.active = index;
+    //   } else {
+    //     angular.element(document.querySelector('#b' + index)).addClass('active');
+    //     angular.element(document.querySelector('#b' + this.previewPos)).removeClass('active');
+    //     this.active = index;
+    //   }
+    //   this.selectClass = Math.floor( Math.random() * (4 - 1) + 1 );
+    //   this.includerPres1 =  index+1;
+    // };
+    // this.nextSlide = (i) => {
+    //   this.includerPres1++;
+    //   console.log(i-1 > this.previewPos);
+    //   if (i % 4 === 0 && (i-1) > this.previewPos) {
+    //     console.log(i, angular.element(document.querySelector('#b' + (i - 1))));
+    //     this.index = this.index + 4;
+    //     this.position = this.position + 100;
+    //     this.moveStyle = "margin-left: -" + this.position + "vw;";
+    //     angular.element(document.querySelector('#b' + i)).addClass('active');
+    //     angular.element(document.querySelector('#b' + this.active)).removeClass('active');
+    //     this.active = this.index;
+    //   } else {
+    //     angular.element(document.querySelector('#b' + i)).addClass('active');
+    //     angular.element(document.querySelector('#b' + this.active)).removeClass('active');
+    //     this.active = i;
+    //   }
+    // };
 
     this.biggerTrueAnswer = () => {
       console.log(angular.element(document.querySelector('#true_answer'))[0].className);
@@ -135,7 +155,7 @@ function presoneController() {
       else {
         angular.element(document.querySelector('#true_answer')).removeClass('bigger');
       }
-};
+    };
 
       this.biggerFalseAnswer = () => {
       if (angular.element(document.querySelector('#false_answer'))[0].className == 'chip choice') {
@@ -145,5 +165,86 @@ function presoneController() {
       else {
         angular.element(document.querySelector('#false_answer')).removeClass('bigger');
       }
+    };
+      this.highlight = () => {
+        console.log("pass");
+        this.light = true;
+    };
+
+    this.index = 0;
+    this.active = 0;
+    this.previewPos = 0;
+    this.position = 0;
+
+    this.next = () => {
+      if (this.index < this.slides.length - 4) {
+        if (this.index + 4 == this.active) {
+          angular.element(document.querySelector('#b' + this.active)).addClass('active');
+        }
+        this.position = this.position + 25;
+        this.moveStyle = "margin-left: -" + this.position + "vw;";
+        this.previewPos = this.index;
+        this.index++;
+        angular.element(document.querySelector('#b' + (this.index))).addClass('pos1');
+        angular.element(document.querySelector('#b' + this.previewPos)).removeClass('pos1');
+      }
+      console.log("next preview:" + this.previewPos, "next index:" + this.index, "next active:" + this.active);
+    };
+    this.preview = () => {
+      if (this.index > 0) {
+        if (angular.element(document.querySelector('#b' + (this.index + 3)))[0].className == "active") {
+          this.active = this.index + 3;
+          angular.element(document.querySelector('#b' + this.active)).removeClass('active');
+        }
+        this.position = this.position - 25;
+        this.moveStyle = "margin-left: -" + this.position + "vw;";
+        this.previewPos = this.active;
+        this.index--;
+        angular.element(document.querySelector('#b' + (this.index))).addClass('pos1');
+        angular.element(document.querySelector('#b' + this.previewPos)).removeClass('pos1');
+      }
+      console.log("preview preview:" + this.previewPos, "preview index:" + this.index, "preview active:" + this.active);
+    };
+    this.select = (i) => {
+      console.log(this.active);
+        if (angular.element(document.querySelector('#b' + i))[0].className != "active" && this.active == i) {
+          angular.element(document.querySelector('#b' + i)).addClass('active');
+          this.active = i;
+        } else {
+          angular.element(document.querySelector('#b' + i)).addClass('active');
+          angular.element(document.querySelector('#b' + this.previewPos)).removeClass('active');
+          this.active = i;
+          this.previewPos = this.active;
+
+          this.index = i;
+        }
+        this.selectClass = Math.floor( Math.random() * (4 - 1) + 1 );
+        this.includerPres1 =  i + 1;
+        console.log("select preview:" + this.previewPos, "select index:" + this.index, "select active:" + this.active, "select i:" + i);
+
+    };
+    this.nextVisibleSlide = (i) => {
+        var posOne = angular.element(document.querySelector('.pos1'))[0].id;
+        posOne = Number(posOne.substr(1, 2));
+        this.includerPres1++;
+        console.log("slide preview:" + this.previewPos, "slide index:" + this.index, "slide active:" + this.active, "slide i:" + i);
+          if ((i - posOne) == 4) {
+            console.log("decalage");
+            // if (this.index + 4 == this.active)
+              angular.element(document.querySelector('#b' + i)).addClass('pos1');
+              this.position = this.position + 100;
+              this.moveStyle = "margin-left: -" + this.position + "vw;";
+            }
+              console.log("pas decalage");
+              angular.element(document.querySelector('#b' + i)).addClass('active');
+              angular.element(document.querySelector('#b' + this.previewPos)).removeClass('active');
+              // this.position = this.position + 25;
+              // this.moveStyle = "margin-left: -" + this.position + "vw;";
+        this.active = i;
+        this.previewPos = i;
+        this.index = i;
+        console.log("slide preview:" + this.previewPos, "slide index:" + this.index, "slide active:" + this.active, "slide i:" + i);
+
+
     };
 }
--- public/js/directives/directiveHighlight.js
+++ public/js/directives/directiveHighlight.js
@@ -4,11 +4,12 @@ function directiveHighlight() {
         link: function() {
             var id =0;
             document.getElementById('zoom_sur').draggable = false;
-            document.getElementById("zoom_area").onmousedown = () => {
+            // document.getElementById('sub_container_answer').draggable = false;
+            document.getElementById('zoom_area').onmousedown = () => {
                 this.xStart = Number(event.clientX) - document.getElementById('zoom_area').offsetLeft;
                 this.yStart = Number(event.clientY) - document.getElementById('zoom_area').offsetTop-50;
                 console.log(this.yStart, Number(event.clientY), document.getElementById('zoom_area').offsetTop);
-                document.getElementById("zoom_area").onmouseup = () => {
+                document.getElementById('zoom_area').onmouseup = () => {
                     this.xEnd = Number(event.clientX) - document.getElementById('zoom_area').offsetLeft;
                     this.yEnd = Number(event.clientY) - document.getElementById('zoom_area').offsetTop-50;
                     // console.log(this.xEnd, this.yEnd);
@@ -26,12 +27,27 @@ function directiveHighlight() {
                     document.getElementById('zoom_area').appendChild(box);
                 };
             };
-            document.getElementsByClassName("highlight").onmousedown = () => {
-              console.log("pass");
-              var el = document.getElementsByClassName("highlight");
-              console.log(el);
-              el = document.getElementById(el.id);
-              document.body.removeChild(el);
+            document.getElementById('sub_container_answer').onmousedown = () => {
+                this.xStart = Number(event.clientX) - document.getElementById('sub_container_answer').offsetLeft;
+                this.yStart = Number(event.clientY) - document.getElementById('sub_container_answer').offsetTop-50;
+                console.log(this.yStart, Number(event.clientY), document.getElementById('sub_container_answer').offsetTop);
+                document.getElementById('sub_container_answer').onmouseup = () => {
+                    this.xEnd = Number(event.clientX) - document.getElementById('sub_container_answer').offsetLeft;
+                    this.yEnd = Number(event.clientY) - document.getElementById('sub_container_answer').offsetTop-50;
+                    // console.log(this.xEnd, this.yEnd);
+                    let width = this.xEnd - this.xStart < 0 ? this.xStart - this.xEnd : this.xEnd - this.xStart;
+                    let height = this.yEnd - this.yStart < 0 ? this.yStart - this.yEnd : this.yEnd - this.yStart;
+                    let xStart = this.xStart > this.xEnd ? this.xEnd : this.xStart;
+                    let yStart = this.yStart > this.yEnd ? this.yEnd : this.yStart;
+
+                    // console.log(width, height);
+                    var box = document.createElement('div');
+                    box.setAttribute('style', 'position: absolute; display: block; top: ' + yStart + 'px; left: ' + xStart + 'px; width: ' + width + 'px; height: ' + height + 'px; background-color: yellow; opacity: .4');
+                    box.className = 'highlight';
+                    box.id = 'divanswer' + id;
+                    id++;
+                    document.getElementById('sub_container_answer').appendChild(box);
+                };
             };
         }
     };

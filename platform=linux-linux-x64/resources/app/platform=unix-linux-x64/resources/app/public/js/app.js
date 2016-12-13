961cefcb5ad77c251b9958fb67da115204eab630
Camjr <cfontainef1@gmail.com>
2016-12-13 17:04:35 +0100

    ajout conclusion

--- platform=linux-linux-x64/resources/app/platform=unix-linux-x64/resources/app/public/js/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('app', ['ngRoute', 'ngAnimate'])
-        .controller('mainController', mainController)
-        .controller('presoneController', presoneController)
-        .controller('parcoursController', parcoursController)
-        .controller('navbarController', navbarController)
-        .controller('parcoursController', parcoursController)
-        .directive('directiveZoom', directiveZoom)
-        .directive('directiveHighlight', directiveHighlight)
-        .config(routes);

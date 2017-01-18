c75ab4435dc8520b5d3463520fc3d0525bd99b2d
daedalus418 <morin.gaelle@gmail.com>
2017-01-18 23:35:10 +0100

    css positions + texts edited

--- app/public/js/controllers/presSixController.js
+++ app/public/js/controllers/presSixController.js
@@ -30,7 +30,7 @@ function presSixController() {
         color: 'turquoise'
     }, {
         image: 'djibouti_small',
-        title: 'Exercices: Légendage',
+        title: 'Exercice: Légendage',
         icon: './assets/icon/globe_turquoise.png',
         color: 'turquoise'
     }, {
@@ -45,7 +45,7 @@ function presSixController() {
         color: 'turquoise'
     }, {
         image: 'recadrage_small',
-        title: 'Exercices: Recadrage',
+        title: 'Exercice: Recadrage',
         icon: './assets/icon/globe_turquoise.png',
         color: 'turquoise'
     }, {
@@ -55,12 +55,12 @@ function presSixController() {
         color: 'bleu'
     }, {
         image: 'maitre_gims_small',
-        title: 'Exercices: Putaclic',
+        title: 'Exercice: Putaclic',
         icon: './assets/icon/globe_turquoise.png',
         color: 'turquoise'
     }, {
         image: 'messi_small',
-        title: 'Exercices: Putaclic',
+        title: 'Exercice: Putaclic',
         icon: './assets/icon/globe_turquoise.png',
         color: 'turquoise'
     }, {
